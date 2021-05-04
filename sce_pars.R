f54cc5baf07d1a252328b8d1ee918bd55b278ba5
Valentijn de Jong <v.m.t.dejong-2@umcutrecht.nl>
2021-05-04 11:22:29 +0200

    Generate simulation parameters.

---
+++ sce_pars.R
@@ -0,0 +1,54 @@
+#' Code book for scenario parameters
+#' 
+#' @param set setting: 1 or 0, 1 = development, 0 = validation
+#' @param n sample size per study
+#' @param beta: coefficient: _0 = intercept, _b = binary variable(s), _c = continuous variable(s)
+#' Note: _c is beta 1, _b is beta 2
+#' @param prev_b prevalence(s) of binary variable(s)
+#' @param mean_c mean(s) of continuous variable(s)
+#' @param sd_c sd(s) of continuous variable(s)
+
+make_pars <- function(n      = 2000,
+                      set    = 1,
+                      beta_0 = -2,
+                      beta_c = c(1),
+                      beta_b = c(1),
+                      prev_b = c(.2),
+                      mean_c = c(0),
+                      sd_c   = c(1),
+                      sce    = 0
+                      ) {
+  list(n = n,
+       set = set,
+       beta_0 = beta_0,
+       beta_b = beta_b,
+       beta_c = beta_c,
+       prev_b = prev_b,
+       mean_c = mean_c,
+       sd_c   = sd_c,
+       sce = sce
+       )
+}
+
+
+make_pars_val <- function(pars, n = pars$n, set = 0,...) {
+  dots <- list(...)
+
+  for (item in names(dots))
+    if (is.null((pars[[item]] <- dots[[item]])))
+      stop("All arguments in ... must be named.")
+
+  pars[["set"]] <- set
+  pars[["n"]] <- n
+  pars
+}
+
+make_outcome_formula <- function(data) {
+  rhs <- grep("x", colnames(data), ignore.case = TRUE)
+  formula(paste("y ~ ", paste(colnames(data)[rhs], collapse = " + "), sep = ""))
+}
+
+
+#' @example 
+#' sce_pars <- make_pars()
+#' val_sce_pars <- make_pars_val(sce_pars)
