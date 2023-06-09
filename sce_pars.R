df16a5972be150bbc566d3dce863142f49185fb4
Valentijn de Jong <v.m.t.dejong-2@umcutrecht.nl>
2023-06-09 16:14:03 +0200

    Updated parameters to allow for extra variable W

--- sce_pars.R
+++ sce_pars.R
@@ -3,30 +3,38 @@
 #' @param set setting: 1 or 0, 1 = development, 0 = validation
 #' @param n sample size per study
 #' @param beta: coefficient: _0 = intercept, _b = binary variable(s), _c = continuous variable(s)
-#' Note: _c is beta 1, _b is beta 2
+#' Note: _c is beta 1, _b is beta 2, _w = continuous variable that is not part of prediction model.
+#' @param dir_w Direction of causal effect of w and x.
 #' @param prev_b prevalence(s) of binary variable(s)
 #' @param mean_c mean(s) of continuous variable(s)
 #' @param sd_c sd(s) of continuous variable(s)
+#' @param trunc_c At what values should c be truncated? c(-Inf, Inf) for no truncation
 
-make_pars <- function(n      = 2000,
-                      set    = 1,
-                      beta_0 = -2,
-                      beta_c = c(1),
-                      beta_b = c(1),
-                      prev_b = c(.2),
-                      mean_c = c(0),
-                      sd_c   = c(1),
-                      sce    = 0
+make_pars <- function(n       = 2000,
+                      set     = 1,
+                      beta_0  = -2,
+                      beta_c  = c(1),
+                      beta_b  = c(1),
+                      beta_w  = NULL,
+                      dir_w   = "x, w",
+                      prev_b  = c(.2),
+                      mean_c  = c(0),
+                      sd_c    = c(1),
+                      trunc_c = c(-Inf, Inf),
+                      sce     = 0
                       ) {
-  list(n = n,
-       set = set,
-       beta_0 = beta_0,
-       beta_b = beta_b,
-       beta_c = beta_c,
-       prev_b = prev_b,
-       mean_c = mean_c,
-       sd_c   = sd_c,
-       sce = sce
+  list(n       = n,
+       set     = set,
+       beta_0  = beta_0,
+       beta_b  = beta_b,
+       beta_c  = beta_c,
+       beta_w  = beta_w,
+       dir_w   = dir_w,
+       prev_b  = prev_b,
+       mean_c  = mean_c,
+       sd_c    = sd_c,
+       trunc_c = trunc_c,
+       sce     = sce
        )
 }
 
